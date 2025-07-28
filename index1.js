4c15b12bbfdbfde0ca7960a52ec6b0aab072c7ee
Ayush Kumar <126682395+ayushkumaroj@users.noreply.github.com>
2025-07-28 09:28:46 +0530

    Adding project file
    along with readme

---
+++ index1.js
@@ -0,0 +1,24 @@
+//creating an http server
+//express
+//node default library => no
+
+const express = require("express");
+const app = express();
+port = 3000
+
+function sum(n){
+  let ans = 0;
+  for(let i=0; i<=n; i++){
+    ans = ans+i;
+  }
+  return ans;
+}
+
+app.get("/", function(req, res){
+    const n = req.query.n;
+    const ans = sum(n)
+    res.send("hi your answer is: "+ ans);
+})
+
+
+app.listen(port);
